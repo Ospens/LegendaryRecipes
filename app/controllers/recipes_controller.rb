848db6ddb03c2c34326970e8a1d93e849be57d71
Ilya <1ospens1@gmail.com>
2018-02-25 22:57:19 +0300

    index root get data from db

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,11 @@
 class RecipesController < ApplicationController
+
   def index
+  	@recipes = Recipe.where(category: 'meat').take(3)
   end
+
+  def show
+  	@recipe = Recipe.find(params[:id])
+  end
+
 end
