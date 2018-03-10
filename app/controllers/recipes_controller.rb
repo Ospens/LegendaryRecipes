c86fd2187598e9bd144e3ada64190d94585d6fa5
Ilya <1ospens1@gmail.com>
2018-03-10 22:20:39 +0300

    add form for create and update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,4 +12,35 @@ class RecipesController < ApplicationController
   	@ingredients = @recipe.ingredients
   end
 
+  def new
+  	@recipe = Recipe.new
+  end
+
+	def create
+		@recipe = Recipe.new(recipe_params)
+		if @recipe.save
+			redirect_to @recipe
+		else
+			render :new
+		end
+	end
+
+  def edit
+  	@recipe = Recipe.find(params[:id])  	
+  end
+
+  def update
+  	@recipe = Recipe.find(params[:id])
+  	if  @recipe.update_attributes(recipe_params)  	
+  		redirect_to @recipe
+  	else
+  		render :edit
+  	end
+  end
+
+  private
+
+  def recipe_params
+		params.require(:recipe).permit(:title, :description, :img, :category_id, :time, :portion)  	
+  end
 end
